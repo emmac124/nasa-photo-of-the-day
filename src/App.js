8d0ddf7706f48b7d5fc8cbfcde841e45345b6323
emmac124 <emma.cooperhawkins@gmail.com>
2021-01-20 15:45:51 -0800

    working on rendering App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import Display from './components/Display';
+import { BASE_URL, API_KEY } from './constants/const';
 import "./App.css";
 
 function App() {
+  
+  const [pageData, setPageData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then((res) => {
+       setPageData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className='App'>
+    <Display pageData={pageData} />
     </div>
   );
 }
